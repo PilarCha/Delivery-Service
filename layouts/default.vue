a5747c30efbfddb03945cd21a33bde4e47fb5cba
PilarCha <pchavez92@gmail.com>
2021-02-05 14:47:21 -0800

    Created Nav bar at the side and removed unecessary buttons on header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -30,32 +30,6 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -103,9 +77,24 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: 'Customer',
+          to: '/customers'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'Orders',
+          to: '/orders'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'Routes',
+          to: '/routes'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'Drivers',
+          to: '/drivers'
+        },
       ],
       miniVariant: false,
       right: true,
