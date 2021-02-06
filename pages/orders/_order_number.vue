92cc2dcc548eda93bfd9f7f40b601f6affa5404b
PilarCha <pchavez92@gmail.com>
2021-02-05 21:28:45 -0800

    Exercise to pass data from parent to child. Goodluck!

--- pages/orders/_order_number.vue
+++ pages/orders/_order_number.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="">
-    <h1>Order Number</h1>
-    {{ orderNumber }}
+    <h1>Order: {{ orderNumber }}</h1>
+    <section>
+      <h2>Details</h2>
+    </section>
   </div>
 </template>
 
