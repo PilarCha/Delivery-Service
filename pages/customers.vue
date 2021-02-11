bb8ae42565d396ba149000b690816013192fbcbf
PilarCha <pchavez92@gmail.com>
2021-02-10 18:50:40 -0800

    added different stores js for each component

--- pages/customers.vue
+++ pages/customers.vue
@@ -2,9 +2,13 @@
   <div class="">
     <h1 class = "mb-4">customers</h1>
     <section>
-      <v-card>
-        <v-card-title> Hello, world </v-card-title>
-        <v-card-subtitle>Subtitle</v-card-subtitle>
+      <v-card
+        :to="`/customer/${customer.customer_id}`"
+        v-for="customer in customers"
+        :key="customer.customer_id"
+      >
+        <v-card-title> {{ customer.customer_id }} </v-card-title>
+        <v-card-subtitle>{{ customer.name }}</v-card-subtitle>
       </v-card>
     </section>
   </div>
@@ -12,10 +16,11 @@
 
 <script>
 import {mapState} from "vuex"
+
 export default {
 
-  computed: mapState(["customers"]),
-  
+  computed: mapState(["customers"])
+
 }
 </script>
 
@@ -23,3 +28,22 @@ export default {
 
 <style lang="css" scoped>
 </style>
+
+<!-- // data () {
+//   return {
+//     customers:[
+//         {
+//           customer_id: 1,
+//           name:'Matt Peepee'
+//         },
+//         {
+//           customer_id: 2,
+//           name: 'Juan won one'
+//         },
+//         {
+//           customer_id:3,
+//           name:'Jumping Juniper'
+//         },
+//     ]
+//   }
+// } -->
