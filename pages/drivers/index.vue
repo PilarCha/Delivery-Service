659a5ccaf4f6918433efad5f275092895b4faebd
PilarCha <pchavez92@gmail.com>
2021-02-11 17:53:05 -0800

    setup directory for drivers pages

---
+++ pages/drivers/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1 class="mb-4">Drivers</h1>
+    <section>
+      <v-card>
+        <v-card-title> Hello, world </v-card-title>
+        <v-card-subtitle>Subtitle</v-card-subtitle>
+      </v-card>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
