7c6e456907bd148a11209741f289ad4e0137608b
PilarCha <pchavez92@gmail.com>
2021-02-20 17:03:10 -0800

    specific driver can be pulled now

--- pages/drivers/_driver_id.vue
+++ pages/drivers/_driver_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <h1 class="mb-4">Drivers</h1>
+    <h1 class="mb-4">Driver Name: {{ driverId.name }}</h1>
     <section>
       <v-card>
         <v-card-title> Hello, world </v-card-title>
@@ -12,6 +12,12 @@
 
 <script>
 export default {
+  computed: {
+    driverId () {
+      let driverId = this.$route.params.driver_id;
+      return this.$store.state.drivers.data.find(driver => driver.driver_id == driverId)
+    }
+  }
 }
 </script>
 
