98440d862f8df535f5a12f8274619bb856992ed0
PilarCha <pchavez92@gmail.com>
2021-02-20 16:48:06 -0800

    forgot to commit

--- pages/drivers/index.vue
+++ pages/drivers/index.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="">
-    <h1 class="mb-4">Drivers</h1>
+    <h1 class = "mb-4">Drivers</h1>
     <section>
-      <v-card>
-        <v-card-title> Hello, world </v-card-title>
-        <v-card-subtitle>Subtitle</v-card-subtitle>
+      <v-card
+        class = "mb-2"
+        :to="`/drivers/${driver.driver_id}`"
+        :key="index"
+        v-for="(driver,index) in drivers"
+      >
+        <v-card-title> {{ driver.name }} </v-card-title>
       </v-card>
     </section>
   </div>
 </template>
 
 <script>
-export default {
-}
-</script>
 
-<style lang="css" scoped>
-</style>
+export default {
+
+  computed: {
+    drivers () {
+      return this.$store.state.drivers.data
+    }
+  }
+
+}
