a65cdc776f6d75b7c035be42845c81b4305afec7
PilarCha <pchavez92@gmail.com>
2021-02-22 21:26:19 -0800

    Starting on the routes page

--- pages/routes/index.vue
+++ pages/routes/index.vue
@@ -2,16 +2,101 @@
   <div class="">
     <h1 class="mb-4">Routes page</h1>
     <section>
-      <v-card>
-        <v-card-title> Hello, world </v-card-title>
-        <v-card-subtitle>Subtitle</v-card-subtitle>
-      </v-card>
+      <v-stepper v-model="steps">
+        <v-stepper-header>
+          <v-stepper-step
+            :complete="steps > 1"
+            step="1"
+          >
+            Name of step 1
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step
+            :complete="steps > 2"
+            step="2"
+          >
+            Name of step 2
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step step="3">
+            Name of step 3
+          </v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <v-card
+              class="mb-12"
+              color="grey lighten-1"
+              height="200px"
+            ></v-card>
+
+            <v-btn
+              color="primary"
+              @click="steps = 2"
+            >
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <v-card
+              class="mb-12"
+              color="grey lighten-1"
+              height="200px"
+            ></v-card>
+
+            <v-btn
+              color="primary"
+              @click="steps = 3"
+            >
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <v-card
+              class="mb-12"
+              color="grey lighten-1"
+              height="200px"
+            ></v-card>
+
+            <v-btn
+              color="primary"
+              @click="steps = 1"
+            >
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
     </section>
   </div>
 </template>
 
 <script>
 export default {
+  data ()  {
+    return {
+      steps: 1,
+    }
+  }
 }
 </script>
 
