5bf07c4d4806bb0b249491534ca04caddcf7cf0a
PilarCha <pchavez92@gmail.com>
2021-02-22 21:08:27 -0800

    got rid of extra html

--- pages/customers/_customer_id.vue
+++ pages/customers/_customer_id.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="">
     <h1 class = "mb-4">Customer Name: {{ customerId.name }}</h1>
-    <section>
-      <v-card
-        class = "mb-2"
-      >
-        <v-card-title>  </v-card-title>
-      </v-card>
-    </section>
   </div>
 
 </template>
--- pages/drivers/_driver_id.vue
+++ pages/drivers/_driver_id.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="">
     <h1 class="mb-4">Driver Name: {{ driverId.name }}</h1>
-    <section>
-      <v-card>
-        <v-card-title> Hello, world </v-card-title>
-        <v-card-subtitle>Subtitle</v-card-subtitle>
-      </v-card>
-    </section>
   </div>
 </template>
 
